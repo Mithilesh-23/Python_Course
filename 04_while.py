df1229079591236f5e73133877491c75fc4ba80b
Mithilesh-23 <mithileshdhandale324@gmail.com>
2025-02-28 09:27:41 +0530

    for loops in python

---
+++ 04_while.py
@@ -0,0 +1,52 @@
+# #eg 1
+# n=1
+# if n!=0:
+#     while n>0:
+#         n=int(input('Enter a number : '))
+#         sq=n*n
+#         print(f'Square of {n} is {sq}')
+# else:
+#     print('square of 0 is 0')
+
+
+# eg 2
+# nm=''
+# while nm!='stop':
+#     nm=input('Enter Name : ')
+#     if nm!='stop':
+#         print(f'Welcome {nm}')
+
+# # eg 3
+# count = 0
+# while count<3:
+#     print(f'c{count}')
+#     count = count + 1
+
+# # eg 4
+# #  Infinite while loop " In this if condition is true then looping is continue infinately "
+# age = 45
+# while age>18:
+#     print("Infinite loop")
+
+# # eg 5
+# # with continue statement
+# num = 0
+# while num <= 10 :
+#     if num==5:
+#         num+=1
+#         continue
+#     else:
+#         print(f'Number is {num}')
+#     num += 1
+
+# # eg 6
+# # with break statement
+# name ='Mithilesh'
+# m=0
+# while m<len(name):
+#     if name[m]== 'e' :
+#         m = m + 1
+#         break
+
+#     print(name[m])
+#     m=m+1
